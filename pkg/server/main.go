d5f56f75c1200fea98b9528c17737a4d3d20052b
Jesse Quinn <me@jessequinn.info>
2019-12-25 15:33:47 -0300

    added env file

--- pkg/server/main.go
+++ pkg/server/main.go
@@ -5,20 +5,23 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jessequinn/go-gql-server/internal/handlers"
+	"github.com/jessequinn/go-gql-server/pkg/utils"
 )
 
 var HOST, PORT string
 
 func init() {
-	HOST = "localhost"
-	PORT = "7777"
+	HOST = utils.MustGet("GQL_SERVER_HOST")
+	PORT = utils.MustGet("GQL_SERVER_PORT")
 }
 
 // Run api server
 func Run() {
 	r := gin.Default()
-	// Setup routes
+	// Simple keep-alive/ping handler
 	r.GET("/ping", handlers.Ping())
+	// Inform the user where the server is listening
 	log.Println("Running @ http://" + HOST + ":" + PORT)
+	// Print out and exit(1) to the OS if the server cannot run
 	log.Fatalln(r.Run(HOST + ":" + PORT))
 }
